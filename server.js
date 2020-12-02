821246f1f4ec19bfcae61a8c43405b37392aae63
danieljrenfro <djrenfro95@gmail.com>
2020-12-02 17:14:18 -0600

    preparing server for production

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-const POKEDEX = require('./pokedex.json')
+const cors = require('cors');
+const helmet = require('helmet');
 
-console.log(process.env.API_TOKEN);
+const POKEDEX = require('./pokedex.json');
 
 const app = express();
-app.use(morgan('common'));
+
+const morganOption = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
-  const authToken = req.get('Authorization')
+  const authToken = req.get('Authorization');
 
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
-    return res.status(401).json({ error: 'Unauthorized request' })
+    return res.status(401).json({ error: 'Unauthorized request' });
   }
   
-  console.log('validate bearer token middleware')
   next();
-})
+});
 
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
 
@@ -26,27 +31,27 @@ function handleGetTypes(req, res) {
   res.json(validTypes);
 }
 
-app.get('/types', handleGetTypes)
+app.get('/types', handleGetTypes);
 
 function handleGetPokemon(req, res) {
   const { name='', type='' } = req.query;
 
   if (!name && !type) {
-    return res.status(400).send('Please search by either a pokemon name or type')
+    return res.status(400).send('Please search by either a pokemon name or type');
   }
 
   if (type && !validTypes.includes(type)) {
-    return res.status(400).send(`Invalid pokemon type. Please select one of the following valid types:\n ${validTypes.join('\n')}`)
+    return res.status(400).send(`Invalid pokemon type. Please select one of the following valid types:\n ${validTypes.join('\n')}`);
   }
 
   let results = POKEDEX.pokemon;
   
   if (type && type !== '') {
-    results = results.filter(poke => poke.type.includes(type))
+    results = results.filter(poke => poke.type.includes(type));
   }
 
   if (name && name !== '') {
-    results = results.filter(poke => poke.name.toLowerCase().includes(name.toLowerCase()))
+    results = results.filter(poke => poke.name.toLowerCase().includes(name.toLowerCase()));
   }
   
   res.send(results);
@@ -54,6 +59,16 @@ function handleGetPokemon(req, res) {
 
 app.get('/pokemon', handleGetPokemon);
 
-const PORT = 8000;
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'Server Error' } };
+  } else {
+    response = { error };
+  }
+  res.status(500).send(response);
+});
 
-app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT}`));
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT);
